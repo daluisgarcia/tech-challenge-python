68a88f3bf40d9dafb5bae91b1c0d11a513b69cca
Daniel Luis <daluisgarcia@gmail.com>
2022-12-22 16:33:55 -0400

    Basic example set as default in challenge

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from stats import DataCapture
 
 if __name__ == '__main__':
     capture = DataCapture()
-    capture.add(1)
-    capture.add(2)
+    capture.add(3)
+    capture.add(9)
     capture.add(3)
     capture.add(4)
-    capture.add(5)
+    capture.add(6)
     stats = capture.build_stats()
 
-    print('Less method:', stats.less(2)) # should return 2 (only two values 3, 3 are less than 4)
-    print('Between method:', stats.between(2, 4)) # should return 4 (3, 3, 4 and 6 are between 3 and 6)
+    print('Less method:', stats.less(4)) # should return 2 (only two values 3, 3 are less than 100)
+    print('Between method:', stats.between(3, 6)) # should return 4 (3, 3, 4 and 6 are between 3 and 6)
     print('Greater method:', stats.greater(4)) # should return 2 (6 and 9 are the only two values greater than 4)
