85190aa9407ddc6c22176dc1f8c3f9630431a34d
Daniel Luis <daluisgarcia@gmail.com>
2023-01-13 13:47:25 -0400

    Comment content fixed

--- main.py
+++ main.py
@@ -10,6 +10,6 @@ if __name__ == '__main__':
     capture.add(6)
     stats = capture.build_stats()
 
-    print('Less method:', stats.less(4)) # should return 2 (only two values 3, 3 are less than 100)
+    print('Less method:', stats.less(4)) # should return 2 (only two values 3, 3 are less than 4)
     print('Between method:', stats.between(3, 6)) # should return 4 (3, 3, 4 and 6 are between 3 and 6)
     print('Greater method:', stats.greater(4)) # should return 2 (6 and 9 are the only two values greater than 4)
