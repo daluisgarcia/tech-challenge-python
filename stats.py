680e76a93653768048b0158e45c7d19e909276b6
Daniel Luis <daluisgarcia@gmail.com>
2023-01-13 13:49:02 -0400

    DataLimit class added and DataStats internal structure changed to lower the complexity

--- stats.py
+++ stats.py
@@ -1,25 +1,37 @@
-class DataStats:
-    def __init__(self, data_list: list[int], max_number: int) -> None:
-        self.__value_limit = 1000 # The maximum value allowed in the data list
-        self.__max_number = max_number + 1 # Adding one to avoid calculation errors when querying the data list
-        self.__data_list: list[int] = [0] * (self.__max_number + 1) # Build a list of 0's the size of the max number in the data
-        
-        for value in data_list:
-            self.__data_list[value] += 1 # Increment the count of the values in the data list
+class DataLimit:
+    value_limit = 1000 # The maximum number value allowed in the data list
 
-        # Builds a list with the amount of numbers less than and greater than each index number
-        self.__data_list = [{'less': sum(self.__data_list[:i]), 'on_side': self.__data_list[i], 'greater': sum(self.__data_list[i+1:])} for i in range(len(self.__data_list))]
+class DataStats:
+    def __init__(self, data_list: list[int]) -> None: # O(n)
+        """Initializes the DataStats class
+
+        Args:
+            data_list (list[int]): A list of integers between 0 and ``DataLimit.value_limit``. The length of the list should be ``DataLimit.value_limit`` + 1.
+        """   
+        self.__value_limit: int = DataLimit.value_limit
+        self.__total_numbers_count: int = 0
+        self.__less_numbers_count_list = dict()
+
+        less_numbers_count = 0 
+        for i in range(len(data_list)):
+
+            self.__total_numbers_count += data_list[i]
+
+            self.__less_numbers_count_list[i] = less_numbers_count
+            less_numbers_count += data_list[i]
+
+        self.__numbers_count = data_list
 
 
     def less(self, number: int) -> int: # O(1)
         """Returns the number of values in the data that are less than the number passed in
 
         Args:
-            number (int): The number to compare the data values to. Must be between 0 and 1000.
+            number (int): The number to compare the data values to. Must be between 0 and ``self.__value_limit``.
 
         Raises:
             TypeError: When the number passed in is not an integer
-            ValueError: When the number passed in is not between 0 and the max number in the data
+            ValueError: When the number passed in is not between 0 and ``self.__value_limit``
 
         Returns:
             int: The number of values in the data that are less than the number passed in
@@ -29,47 +41,15 @@ class DataStats:
 
         if number < 0 or number > self.__value_limit:
             raise ValueError('Number must be between 0 and {}'.format(self.__value_limit))
-        
-        if number > self.__max_number: # Avoid index out of range error when querying the data list
-            number = self.__max_number
 
-        return self.__data_list[number]['less']
-
-
-    def between(self, low_number: int, high_number: int = 0) -> int: # O(1)
-        """Returns the number of values in the data that are between the low number and high number passed in
-
-        Args:
-            low_number (int): The low number to compare the data values to. Must be between 0 and 1000.
-            high_number (int, optional): The high number to compare the data values to. Must be between 0 and 1000. Defaults to 0.
-
-        Raises:
-            TypeError: When any of the numbers passed in are not an integer
-            ValueError: When any of the numbers passed in are not between 0 and the max number in the data
-
-        Returns:
-            int: The number of values in the data that are between the low number and high number passed in
-        """
-        if not isinstance(low_number, int) or not isinstance(high_number, int):
-            raise TypeError('The numbers must be an integer')
-
-        if low_number < 0 or low_number > self.__value_limit or high_number < 0 or high_number > self.__value_limit:
-            raise ValueError('Number must be between 0 and {}'.format(self.__value_limit))
-
-        if low_number >= high_number:
-            return 0
-
-        if high_number > self.__max_number: # Avoid index out of range error when querying the data list
-            high_number = self.__max_number
-
-        return self.__data_list[high_number]['less'] - self.__data_list[low_number]['less'] + self.__data_list[high_number]['on_side']
+        return self.__less_numbers_count_list[number]
 
 
     def greater(self, number: int) -> int: # O(1)
         """Returns the number of values in the data list that are greater than the number passed in
 
         Args:
-            number (int): The number to compare the data list values to. Must be between 0 and 1000.
+            number (int): The number to compare the data list values to. Must be between 0 and ``self.__value_limit``.
 
         Raises:
             TypeError: When the number passed in is not an integer
@@ -84,27 +64,50 @@ class DataStats:
         if number < 0 or number > self.__value_limit:
             raise ValueError('Number must be between 0 and {}'.format(self.__value_limit))
 
-        if number > self.__max_number: # Avoid index out of range error when querying the data list
-            number = self.__max_number
+        return self.__total_numbers_count - self.__less_numbers_count_list[number] - self.__numbers_count[number]
 
-        return self.__data_list[number]['greater']
+
+    def between(self, low_number: int, high_number: int = DataLimit.value_limit) -> int: # O(1)
+        """Returns the number of values in the data that are between the low number and high number passed in
+
+        Args:
+            low_number (int): The low number to compare the data values to. Must be between 0 and ``self.__value_limit``.
+            high_number (int, optional): The high number to compare the data values to. Must be between 0 and ``self.__value_limit``. Defaults to ``DataLimit.value_limit``.
+
+        Raises:
+            TypeError: When any of the numbers passed in are not an integer
+            ValueError: When any of the numbers passed in are not between 0 and value limit
+
+        Returns:
+            int: The number of values in the data that are between the low number and high number passed in
+        """
+        if not isinstance(low_number, int) or not isinstance(high_number, int):
+            raise TypeError('The numbers must be an integer')
+
+        if low_number < 0 or low_number > self.__value_limit or high_number < 0 or high_number > self.__value_limit:
+            raise ValueError('Number must be between 0 and {}'.format(self.__value_limit))
+
+        if low_number >= high_number:
+            return 0
+
+        return self.__total_numbers_count - self.less(low_number) - self.greater(high_number)
 
 
 class DataCapture:
-    def __init__(self) -> None:
-        self.__data_list: list[int] = list()
-        self.__value_limit = 1000 # The maximum value allowed in the data list
+    def __init__(self) -> None:        
+        self.__value_limit = DataLimit.value_limit
+        self.__data_list: list[int] = [0] * (self.__value_limit + 1) # Build a list of zeros with the value limit as the size
 
 
     def add(self, number: int): # O(1)
         """Adds a number to the data list and sorts the data list
 
         Args:
-            number (int): The number to add to the data list. Must be between 0 and 1000.
+            number (int): The number to add to the data list. Must be between 0 and ``self.__value_limit``.
 
         Raises:
             TypeError: When the number passed in is not an integer
-            ValueError: When the number passed in is not between 0 and 1000
+            ValueError: When the number passed in is not between 0 and ``self.__value_limit``
         """
         if not isinstance(number, int):
             raise TypeError('The number must be an integer')
@@ -112,7 +115,7 @@ class DataCapture:
         if number < 0 or number > self.__value_limit:
             raise ValueError('Number must be positive and less than {}'.format(self.__value_limit))
         
-        self.__data_list.append(number)
+        self.__data_list[number] += 1 # Increment the count of the number in the data list
 
 
     def build_stats(self) -> DataStats: # O(n)
@@ -121,4 +124,4 @@ class DataCapture:
         Returns:
             DataStats: A DataStats object created with the data list
         """
-        return DataStats(self.__data_list, max(self.__data_list))
+        return DataStats(self.__data_list)
--- tests/test_data_stats.py
+++ tests/test_data_stats.py
@@ -1,162 +1,284 @@
 import pytest
 
-from stats import DataStats
+from stats import DataStats, DataLimit
 
 
 class TestDataStatsLessMethod:
+    def __init_number_count(self) -> list[int]:
+        return [0] * (DataLimit.value_limit + 1)
+
 
     def test_less_method_with_no_repeated_and_no_left(self):
-        data_list = [1, 2, 3, 4, 5]
-        data = DataStats(data_list, max(data_list))
+        number_count = self.__init_number_count()
+        number_count[1] = 1
+        number_count[2] = 1
+        number_count[3] = 1
+        number_count[4] = 1
+        number_count[5] = 1
+        data = DataStats(number_count)
         assert data.less(3) == 2
 
 
     def test_less_method_with_some_repeated_and_no_left(self):
-        data_list = [1, 1, 2, 3, 3, 4, 5]
-        data = DataStats(data_list, max(data_list))
+        number_count = self.__init_number_count()
+        number_count[1] = 2
+        number_count[2] = 1
+        number_count[3] = 2
+        number_count[4] = 1
+        number_count[5] = 1
+        data = DataStats(number_count)
         assert data.less(3) == 3
 
 
     def test_less_method_with_some_repeated_and_some_left(self):
-        data_list = [1, 2, 3, 3, 4, 4, 5, 5]
-        data = DataStats(data_list, max(data_list))
+        number_count = self.__init_number_count()
+        number_count[1] = 1
+        number_count[2] = 1
+        number_count[3] = 2
+        number_count[4] = 2
+        number_count[5] = 2
+        data = DataStats(number_count)
         assert data.less(2) == 1
 
 
     def test_less_method_with_no_left_and_all_repeated(self):
-        data_list = [1, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5]
-        data = DataStats(data_list, max(data_list))
+        number_count = self.__init_number_count()
+        number_count[1] = 3
+        number_count[2] = 2
+        number_count[3] = 2
+        number_count[4] = 2
+        number_count[5] = 2
+        data = DataStats(number_count)
         assert data.less(3) == 5
 
 
     def test_less_method_with_some_left_and_all_repeated(self):
-        data_list = [1, 1, 1, 1, 2, 2, 4, 4, 4, 5, 5]
-        data = DataStats(data_list, max(data_list))
+        number_count = self.__init_number_count()
+        number_count[1] = 4
+        number_count[2] = 2
+        number_count[4] = 3
+        number_count[5] = 2
+        data = DataStats(number_count)
         assert data.less(100) == 11
 
 
     def test_less_method_raises_type_error(self):
-        data_list = [1, 2, 3, 4, 5]
-        data = DataStats(data_list, max(data_list))
+        number_count = self.__init_number_count()
+        number_count[1] = 1
+        number_count[2] = 1
+        number_count[3] = 1
+        number_count[4] = 1
+        number_count[5] = 1
+        data = DataStats(number_count)
         with pytest.raises(TypeError):
             data.less('a')
 
 
     def test_less_method_raises_value_error(self):
-        data_list = [1, 2, 3, 4, 5]
-        data = DataStats(data_list, max(data_list))
+        number_count = self.__init_number_count()
+        number_count[1] = 1
+        number_count[2] = 1
+        number_count[3] = 1
+        number_count[4] = 1
+        number_count[5] = 1
+        data = DataStats(number_count)
         with pytest.raises(ValueError):
             data.less(1001)
 
 
     def test_less_method_raises_value_error_negative_number(self):
-        data_list = [1, 2, 3, 4, 5]
-        data = DataStats(data_list, max(data_list))
+        number_count = self.__init_number_count()
+        number_count[1] = 1
+        number_count[2] = 1
+        number_count[3] = 1
+        number_count[4] = 1
+        number_count[5] = 1
+        data = DataStats(number_count)
         with pytest.raises(ValueError):
             data.less(-1)
 
 
 class TestDataStatsBetweenMethod:
+    def __init_number_count(self) -> list[int]:
+        return [0] * (DataLimit.value_limit + 1)
     
     def test_between_method_with_no_repeated_and_no_left(self):
-        data_list = [1, 2, 3, 4, 5]
-        data = DataStats(data_list, max(data_list))
+        number_count = self.__init_number_count()
+        number_count[1] = 1
+        number_count[2] = 1
+        number_count[3] = 1
+        number_count[4] = 1
+        number_count[5] = 1
+        data = DataStats(number_count)
         assert data.between(2, 4) == 3
 
 
     def test_between_method_with_some_repeated_and_no_left(self):
-        data_list = [1, 1, 2, 3, 3, 4, 5, 5]
-        data = DataStats(data_list, max(data_list))
+        number_count = self.__init_number_count()
+        number_count[1] = 2
+        number_count[2] = 1
+        number_count[3] = 2
+        number_count[4] = 1
+        number_count[5] = 2
+        data = DataStats(number_count)
         assert data.between(2, 4) == 4
 
 
     def test_between_method_with_some_repeated_and_some_left(self):
-        data_list = [1, 1, 2, 3, 3, 5, 5]
-        data = DataStats(data_list, max(data_list))
+        number_count = self.__init_number_count()
+        number_count[1] = 2
+        number_count[2] = 1
+        number_count[3] = 2
+        number_count[5] = 2
+        data = DataStats(number_count)
         assert data.between(2, 4) == 3
 
     
     def test_between_method_with_no_left_and_all_repeated(self):
-        data_list = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5]
-        data = DataStats(data_list, max(data_list))
+        number_count = self.__init_number_count()
+        number_count[1] = 2
+        number_count[2] = 2
+        number_count[3] = 2
+        number_count[4] = 2
+        number_count[5] = 2
+        data = DataStats(number_count)
         assert data.between(2, 4) == 6
 
 
     def test_between_method_with_some_left_and_all_repeated(self):
-        data_list = [1, 1, 2, 2, 4, 4, 5, 5]
-        data = DataStats(data_list, max(data_list))
+        number_count = self.__init_number_count()
+        number_count[1] = 2
+        number_count[2] = 2
+        number_count[4] = 2
+        number_count[5] = 2
+        data = DataStats(number_count)
         assert data.between(2, 1000) == 6
 
 
     def test_between_method_raises_type_error(self):
-        data_list = [1, 2, 3, 4, 5]
-        data = DataStats(data_list, max(data_list))
+        number_count = self.__init_number_count()
+        number_count[1] = 1
+        number_count[2] = 1
+        number_count[3] = 1
+        number_count[4] = 1
+        number_count[5] = 1
+        data = DataStats(number_count)
         with pytest.raises(TypeError):
             data.between('a', 1)
 
     
     def test_between_method_raises_value_error(self):
-        data_list = [1, 2, 3, 4, 5]
-        data = DataStats(data_list, max(data_list))
+        number_count = self.__init_number_count()
+        number_count[1] = 1
+        number_count[2] = 1
+        number_count[3] = 1
+        number_count[4] = 1
+        number_count[5] = 1
+        data = DataStats(number_count)
         with pytest.raises(ValueError):
             data.between(3, 1001)
 
     
     def test_between_method_raises_value_error_negative_number(self):
-        data_list = [1, 2, 3, 4, 5]
-        data = DataStats(data_list, max(data_list))
+        number_count = self.__init_number_count()
+        number_count[1] = 1
+        number_count[2] = 1
+        number_count[3] = 1
+        number_count[4] = 1
+        number_count[5] = 1
+        data = DataStats(number_count)
         with pytest.raises(ValueError):
             data.between(-1, 7)
 
 
 class TestDataStatsGreaterMethod:
+    def __init_number_count(self) -> list[int]:
+        return [0] * (DataLimit.value_limit + 1)
     
     def test_greater_method_with_no_repeated_and_no_left(self):
-        data_list = [1, 2, 3, 4, 5]
-        data = DataStats(data_list, max(data_list))
+        number_count = self.__init_number_count()
+        number_count[1] = 1
+        number_count[2] = 1
+        number_count[3] = 1
+        number_count[4] = 1
+        number_count[5] = 1
+        data = DataStats(number_count)
         assert data.greater(3) == 2
 
 
     def test_greater_method_with_some_repeated_and_no_left(self):
-        data_list = [1, 1, 2, 3, 3, 4, 5, 5]
-        data = DataStats(data_list, max(data_list))
+        number_count = self.__init_number_count()
+        number_count[1] = 2
+        number_count[2] = 1
+        number_count[3] = 2
+        number_count[4] = 1
+        number_count[5] = 2
+        data = DataStats(number_count)
         assert data.greater(3) == 3
 
 
     def test_greater_method_with_some_repeated_and_some_left(self):
-        data_list = [1, 1, 2, 3, 3, 5, 5]
-        data = DataStats(data_list, max(data_list))
+        number_count = self.__init_number_count()
+        number_count[1] = 2
+        number_count[2] = 1
+        number_count[3] = 2
+        number_count[5] = 2
+        data = DataStats(number_count)
         assert data.greater(3) == 2
 
 
     def test_greater_method_with_no_left_and_all_repeated(self):
-        data_list = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5]
-        data = DataStats(data_list, max(data_list))
+        number_count = self.__init_number_count()
+        number_count[1] = 2
+        number_count[2] = 2
+        number_count[3] = 2
+        number_count[4] = 2
+        number_count[5] = 2
+        data = DataStats(number_count)
         assert data.greater(3) == 4
 
 
     def test_greater_method_with_some_left_and_all_repeated(self):
-        data_list = [1, 1, 2, 2, 4, 4, 5, 5]
-        data = DataStats(data_list, max(data_list))
+        number_count = self.__init_number_count()
+        number_count[1] = 2
+        number_count[2] = 2
+        number_count[4] = 2
+        number_count[5] = 2
+        data = DataStats(number_count)
         assert data.greater(2) == 4
 
 
     def test_greater_method_raises_type_error(self):
-        data_list = [1, 2, 3, 4, 5]
-        data = DataStats(data_list, max(data_list))
+        number_count = self.__init_number_count()
+        number_count[1] = 1
+        number_count[2] = 1
+        number_count[3] = 1
+        number_count[4] = 1
+        number_count[5] = 1
+        data = DataStats(number_count)
         with pytest.raises(TypeError):
             data.greater('a')
 
 
     def test_greater_method_raises_value_error(self):
-        data_list = [1, 2, 3, 4, 5]
-        data = DataStats(data_list, max(data_list))
+        number_count = self.__init_number_count()
+        number_count[1] = 1
+        number_count[2] = 1
+        number_count[3] = 1
+        number_count[4] = 1
+        number_count[5] = 1
+        data = DataStats(number_count)
         with pytest.raises(ValueError):
             data.greater(1001)
     
 
     def test_greater_method_raises_value_error_negative_number(self):
-        data_list = [1, 2, 3, 4, 5]
-        data = DataStats(data_list, max(data_list))
+        number_count = self.__init_number_count()
+        number_count[1] = 1
+        number_count[2] = 1
+        number_count[3] = 1
+        number_count[4] = 1
+        number_count[5] = 1
+        data = DataStats(number_count)
         with pytest.raises(ValueError):
             data.greater(-1)
